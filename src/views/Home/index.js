5f9cf98486f472c07e7b5251c798b83f56cb9ab4
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-26 02:57:00 -0400

    changed background for testing

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom'
 
 import HoursContainer from '../../components/HoursContainer'
 import StoreInfoContainer from '../../components/StoreInfoContainer'
 import './home.css'
 
-const Home = () => (
-   <div id="home_wrapper">
-    <div className="col_1">
-      <h1>Our Story</h1>
-      <p>Shears Salon was created by brother and sister team, Michael and Hannah Wrynn.</p>
-      <p>We're focused on delivering high-end salon quality services and products, without the high-end attitude... just good natured, knowledgeable professionals performing the services they love.</p>
-      <p>We aim to be New York's "one-stop-beauty-shop" by offering a full menu of hair, nail. eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
-      <p>While internal beauty will always reign supreme, we wholeheartedly believe that when people look better on the outside, they feel better on the inside. If we can improve your quality of life through the services we provide, then we have completed our mission. </p>
-      <p>You take care of the inner beauty...leave the rest to us. </p>
-     </div>
+class Home extends Component {
+  render() {
+    return (
+       <div id="home_wrapper">
+        <div className="col_1">
+          <h1>Our Story</h1>
+          <p>Shears Salon was created by brother and sister team, Michael and Hannah Wrynn.</p>
+          <p>We're focused on delivering high-end salon quality services and products, without the high-end attitude... just good natured, knowledgeable professionals performing the services they love.</p>
+          <p>We aim to be New York's "one-stop-beauty-shop" by offering a full menu of hair, nail. eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
+          <p>While internal beauty will always reign supreme, we wholeheartedly believe that when people look better on the outside, they feel better on the inside. If we can improve your quality of life through the services we provide, then we have completed our mission. </p>
+          <p>You take care of the inner beauty...leave the rest to us. </p>
+         </div>
 
-     <div className="col_2">
-      <HoursContainer />
-      <StoreInfoContainer />
-      </div>
-   </div>
-)
+         <div className="col_2">
+          <HoursContainer />
+          <StoreInfoContainer />
+          </div>
+       </div>
+     )}
+   }
 
 export default Home
--- src/views/Rates/index.js
+++ src/views/Rates/index.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom'
 
 import HoursContainer from '../../components/HoursContainer'
 import StoreInfoContainer from '../../components/StoreInfoContainer'
 import './rates.css'
 
-const Rates = () => (
-   <div id="rates_wrapper">
-    <div className="col_1">
-      <h1>Our Rates</h1>
-      <p>Here you will find the rates for our Shears services.</p>
-      <p className="service"><strong>Cuts and Hair Styling</strong> <span className="price">$35+</span> <br /><span className="service_description"> Includes styling, finishing, and product for both Men & Women.</span></p>
-      <p className="service"><strong>Colors & Highlights</strong> <span className="price">$60+</span> <br /> <span className="service_description">Single Process, Highlights, and Gloss - varied selection of colors and treatments!</span></p>
-      <p className="service"><strong>Nails</strong> <span className="price">$15+</span> <br /> <span className="service_description">Manicures, Pedicures, and Packages available for selection.</span></p>
-      <p className="service"><strong>Spa Services</strong> <span className="price">$105+</span> <br /><span className="service_description">Deep Tissue Massages, Aromatherapy, and hydrating Facials to renew your energy.</span></p>
-      <p className="service"><strong>Waxing</strong> <span className="price">$25+</span> <br /><span className="service_description">We provide waxing services for face, body, and bikini. French Cirepil wax is used for all services.</span></p>
-      <p> All special requests can be accomodated. Call for more details!</p>
-     </div>
+class Rates extends Component {
+  render() {
+    return (
+       <div id="rates_wrapper">
+        <div className="col_1">
+          <h1>Our Rates</h1>
+          <p>Here you will find the rates for our Shears services.</p>
+          <p className="service"><strong>Cuts and Hair Styling</strong> <span className="price">$35+</span> <br /><span className="service_description"> Includes styling, finishing, and product for both Men & Women.</span></p>
+          <p className="service"><strong>Colors & Highlights</strong> <span className="price">$60+</span> <br /> <span className="service_description">Single Process, Highlights, and Gloss - varied selection of colors and treatments!</span></p>
+          <p className="service"><strong>Nails</strong> <span className="price">$15+</span> <br /> <span className="service_description">Manicures, Pedicures, and Packages available for selection.</span></p>
+          <p className="service"><strong>Spa Services</strong> <span className="price">$105+</span> <br /><span className="service_description">Deep Tissue Massages, Aromatherapy, and hydrating Facials to renew your energy.</span></p>
+          <p className="service"><strong>Waxing</strong> <span className="price">$25+</span> <br /><span className="service_description">We provide waxing services for face, body, and bikini. French Cirepil wax is used for all services.</span></p>
+          <p> All special requests can be accomodated. Call for more details!</p>
+         </div>
 
-     <div className="col_2">
-      <HoursContainer />
-      <StoreInfoContainer />
-      </div>
-   </div>
-)
+         <div className="col_2">
+          <HoursContainer />
+          <StoreInfoContainer />
+          </div>
+       </div>
+    )}
+    }
 
 export default Rates
