1931ce1ca3cc11719b04a7b2f6a89f7eda1bb1df
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-26 00:37:15 -0400

    Added bottom right container to home

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 import './home.css'
 
 const Home = () => (
@@ -6,8 +7,8 @@ const Home = () => (
     <div className="col_1">
       <h1>Our Story</h1>
       <p>Shears Salon was created by brother and sister team, Michael and Hannah Wrynn.</p>
-      <p>The business is focused on delivering high-end salon quality services and products, without the high-end attitude... just good natured, knowledgeable professionals performing the services they love.</p>
-      <p>We aim to be New Yorks "one-stop-beauty-shop" by offering a full menu of hair, nail. eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
+      <p>We're focused on delivering high-end salon quality services and products, without the high-end attitude... just good natured, knowledgeable professionals performing the services they love.</p>
+      <p>We aim to be New York's "one-stop-beauty-shop" by offering a full menu of hair, nail. eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
       <p>While internal beauty will always reign supreme, we wholeheartedly believe that when people look better on the outside, they feel better on the inside. If we can improve your quality of life through the services we provide, then we have completed our mission. </p>
       <p>You take care of the inner beauty...leave the rest to us. </p>
      </div>
@@ -25,7 +26,12 @@ const Home = () => (
        </div>
 
        <div id="store_info_container">
-        <h2>Shears Salon & Designs</h2>
+        <h2>Shears Salon & Designs <i className="fa fa-map-marker" aria-hidden="true"></i></h2>
+        <p>51 Church St <br/>
+        Chatham, NY 12037 <br/>
+        (518) 392-3550
+        </p>
+        <NavLink to="/home">View on the Map</NavLink>
        </div>
       </div>
    </div>
