d21a05efc659df6d8a89eda9a236ae81569c9268
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-26 11:28:51 -0400

    fixed media query on gallery resize

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -13,9 +13,9 @@ class Home extends Component {
           <h1>Our Story</h1>
           <p>Shears Salon was created by brother and sister team, Michael and Hannah Wrynn.</p>
           <p>We're focused on delivering high-end salon quality services and products, without the high-end attitude... just good natured, knowledgeable professionals performing the services they love.</p>
-          <p>We aim to be New York's "one-stop-beauty-shop" by offering a full menu of hair, nail. eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
+          <p>We aim to be New York's "one-stop-beauty-shop" by offering a full menu of hair, nail, eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
           <p>While internal beauty will always reign supreme, we wholeheartedly believe that when people look better on the outside, they feel better on the inside. If we can improve your quality of life through the services we provide, then we have completed our mission. </p>
-          <p>You take care of the inner beauty...leave the rest to us. </p>
+          <p>You take care of the inner beauty... leave the rest to us. </p>
          </div>
 
          <div className="col_2">
