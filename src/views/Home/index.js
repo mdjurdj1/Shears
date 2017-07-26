a2731e3be90651b0c11bdd69d099a5ee05139657
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-26 00:15:37 -0400

    added store info container

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -13,15 +13,21 @@ const Home = () => (
      </div>
 
      <div className="col_2">
-      <h2>Opening Hours</h2>
-      <p><span className="weekday">Monday</span>: <span className="time">Closed</span></p>
-      <p><span className="weekday">Tuesday</span>: <span className="time">9:00am - 5:30pm</span></p>
-      <p><span className="weekday">Wednesday</span>: <span className="time">9:00am - 7:30pm</span></p>
-      <p><span className="weekday">Thursday</span>: <span className="time">9:00am - 7:30pm</span></p>
-      <p><span className="weekday">Friday</span>: <span className="time">9:00am - 7:30pm</span></p>
-      <p><span className="weekday">Saturday</span>: <span className="time">10:30am - 6:00pm</span></p>
-      <p><span className="weekday">Sunday</span>: <span className="time">Closed</span></p>
-     </div>
+       <div id="hours_container">
+        <h2>Opening Hours</h2>
+        <p><span className="weekday">Monday</span>: <span className="time">Closed</span></p>
+        <p><span className="weekday">Tuesday</span>: <span className="time">9:00am - 5:30pm</span></p>
+        <p><span className="weekday">Wednesday</span>: <span className="time">9:00am - 7:30pm</span></p>
+        <p><span className="weekday">Thursday</span>: <span className="time">9:00am - 7:30pm</span></p>
+        <p><span className="weekday">Friday</span>: <span className="time">9:00am - 7:30pm</span></p>
+        <p><span className="weekday">Saturday</span>: <span className="time">10:30am - 6:00pm</span></p>
+        <p><span className="weekday">Sunday</span>: <span className="time">Closed</span></p>
+       </div>
+
+       <div id="store_info_container">
+        <h2>Shears Salon & Designs</h2>
+       </div>
+      </div>
    </div>
 )
 
