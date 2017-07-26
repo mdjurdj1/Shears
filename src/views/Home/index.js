046aa03a0fb09f60a1c282cf19c57a6e0548dca8
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-25 23:24:35 -0400

    added home page

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './home.css'
 
 const Home = () => (
-   <h1>HEY</h1>
+   <div id="home_wrapper">
+    <div className="col_1">
+      <h1>Our Story</h1>
+      <p>Shears Salon was created by brother and sister team, Michael and Mylinh On.</p>
+      <p>The business is focused on delivering high-end salon quality services and products, without the high-end attitude... just good natured, knowledgeable professionals performing the services they love.</p>
+      <p>We aim to be New York's "one-stop-beauty-shop" by offering a full menu of hair, nail. eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
+      <p>While internal beauty will always reign supreme, we wholeheartedly believe that when people look better on the outside, they feel better on the inside. If we can improve your quality of life through the services we provide, then we have completed our mission. </p>
+      <p>You take care of the inner beauty...leave the rest to us. </p>
+     </div>
+   </div>
 )
 
 export default Home
