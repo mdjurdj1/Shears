068f7359b7ea4a845f1f379b060206ad9858155b
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-25 23:59:25 -0400

    added some home page styling

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -5,11 +5,22 @@ const Home = () => (
    <div id="home_wrapper">
     <div className="col_1">
       <h1>Our Story</h1>
-      <p>Shears Salon was created by brother and sister team, Michael and Mylinh On.</p>
+      <p>Shears Salon was created by brother and sister team, Michael and Hannah Wrynn.</p>
       <p>The business is focused on delivering high-end salon quality services and products, without the high-end attitude... just good natured, knowledgeable professionals performing the services they love.</p>
-      <p>We aim to be New York's "one-stop-beauty-shop" by offering a full menu of hair, nail. eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
+      <p>We aim to be New Yorks "one-stop-beauty-shop" by offering a full menu of hair, nail. eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
       <p>While internal beauty will always reign supreme, we wholeheartedly believe that when people look better on the outside, they feel better on the inside. If we can improve your quality of life through the services we provide, then we have completed our mission. </p>
       <p>You take care of the inner beauty...leave the rest to us. </p>
+     </div>
+
+     <div className="col_2">
+      <h2>Opening Hours</h2>
+      <p><span className="weekday">Monday</span>: <span className="time">Closed</span></p>
+      <p><span className="weekday">Tuesday</span>: <span className="time">9:00am - 5:30pm</span></p>
+      <p><span className="weekday">Wednesday</span>: <span className="time">9:00am - 7:30pm</span></p>
+      <p><span className="weekday">Thursday</span>: <span className="time">9:00am - 7:30pm</span></p>
+      <p><span className="weekday">Friday</span>: <span className="time">9:00am - 7:30pm</span></p>
+      <p><span className="weekday">Saturday</span>: <span className="time">10:30am - 6:00pm</span></p>
+      <p><span className="weekday">Sunday</span>: <span className="time">Closed</span></p>
      </div>
    </div>
 )
