a4ad68c9bde65faf7e6a3100fd3c511c309be708
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-28 03:59:48 -0400

    added products page

--- src/views/Products/index.js
+++ src/views/Products/index.js
@@ -8,50 +8,29 @@ import './products.css'
 class Products extends Component {
   render() {
     return (
-       <div id="rates_wrapper">
-        <div className="col_1">
-          <h1>Our Rates</h1>
-          <p className="service"><p className="service_header">HAIR SCULPTING <span className="purp">&</span> SYLING</p>
-            <span className="service_description"> Lady’s Haircuts $40-$100</span> <br />
-            <span className="service_description"> Gentelmens Haircuts $20-$80</span> <br />
-            <span className="service_description"> Childrens Haircuts $20</span> <br /> <br />
-            <span className="service_description"> Hair Braiding $25+</span> <br />
-            <span className="service_description"> Blowout $35+</span> <br />
-            <span className="service_description"> Updo $75+</span> <br />
-            <span className="service_description"> Shears Signature Blowout & Make up Package-Deal $100+</span> <br />
-            <span className="service_description"> Extensions Priced Upon Consultation</span> <br />
-          </p>
+       <div id="products_wrapper">
+        <div className="col_3">
 
-          <p className="service"><p className="service_header">HAIR COLORS <span className="purp">&</span> HIGHLIGHTS</p>
-            <span className="service_description"> Single Process $80+</span> <br />
-            <span className="service_description"> Roots Touch Up $50+</span> <br />
-            <span className="service_description"> Double Process $120+</span> <br /> <br />
-            <span className="service_description"> Color Correction - Priced Upon Consultation</span> <br />
-            <span className="service_description"> Color Consultation - Complementary</span> <br />
-          </p>
+          <div className="products_description">
+            <h1>Shampoos <span className="purp">&</span> Conditioners</h1>
+            <img id="shampoos_pic" src={require('../../images/shampoos.png')} alt="Shampoos Picture" style={{width:400, height:400}} />
+              <p>Shampoo is arguably your most important haircare product and the foundation for great hair. Here at Shears, we offer a variety of Lanza shampoos so that you can find the hair cleanser that meets your core haircare concern—think sulfate-free shampoo for colored hair, moisturizing shampoo for dry hair, or shampoo for damaged hair—as well as the hair shampoos that help you create different end looks such as volume, shine, or frizz control. And for curly girls, find the best shampoo for your curly hair.
+              </p>
+              <p className="quote"><i className="fa fa-quote-left" aria-hidden="true"/>&nbsp; As the seasons change, our hair automatically reacts to the weather as well. Frizzy hair in the summer humidity or dry hair in the harsh winter is no good. Koby believes that hair should be treated with the same detail as a designer approaches different types of fabrics. We wouldn’t wash our cashmere clothes the same way as wool or cotton, right? Each fabric will react differently to water temperature, cleansers, and friction. Same is with our hair and product usage!&nbsp; <i className="fa fa-quote-right" aria-hidden="true"/></p>
+           </div>
 
-          <p className="service"><p className="service_header">BRIDAL MAKEUP <span className="purp">&</span> HAIR</p>
-            <span className="service_description"> Makeup Application $150 ($250+ on location)</span> <br />
-            <span className="service_description"> Touch Up Application $65+</span> <br />
-            <span className="service_description"> Bridal Makeup $200 ($400+ on location)</span> <br /> <br />
-            <span className="service_description"> Bridal Hair $200 ($400+ on location)</span> <br />
-            <span className="service_description">*For private bridal parties/events inquiries, please email ShearsNYC@gmail.com for more information.</span> <br />
-          </p>
+           <div className="products_description">
+             <h1>Remington Styling Tools</h1>
+             <img id="dryer" src={require('../../images/dryer.png')} alt="Dryer Picture" style={{width:200, height:200}} />
+             <img id="curler" src={require('../../images/curler.png')} alt="Curler Picture" style={{width:200, height:200}} />
+             <img id="curler_2" src={require('../../images/curler_2.png')} alt="Curler_2 Picture" style={{width:200, height:200}} />
+               <p>After years of working in the industry, we have excitedly found the tools we feel work best for stylists and clients. Guaranteed to be the lightest/silent and strongest blow dryer you’ve ever owned, Epic Lite is the blowdryer that will change your hair routine. This lightweight next generation hair dryer purifies the outgoing airflow and fights the aging effects of harmful free radicals caused by carbon dioxide and reduce by 40% your hair dry time.
+               In addition to the Epic Lite blow dryer  The rounded plates on this tools make it versatile for straightening stubborn hair, curling or making natural beach waves
+               </p>
+               <p>We believe in the concept and quality of Remington and therefore offer a full 2 year warranty when purchased through Shears. If you are interested in purchasing an Epic Lite blow dryer, flat iron or combination package for you (or as a gift), we offer guaranteed shipping. Stop by Shears today or contact us at ShearsNYC@gmail.com for more information.</p>
+            </div>
 
-          <p className="service"><strong>
-            *Prices may vary by stylist <br/>
-            *All services include our signature blow out & finished style <br />
-            *24 hour courtesy cancellation policy <br />
-            *Hair consultation-complementary <br />
-            *Gratuity is cash only <br />
-            Shears gift certificates are now available!<br/>
-            </strong>
-          </p>
-         </div>
-
-         <div className="col_2">
-            <img id="bridal_pic" src={require('../../images/long_image.jpg')} alt="Bridal Picture" style={{width:400, height:700}} />
-          </div>
+        </div>
        </div>
     )}
     }
