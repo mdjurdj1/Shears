5fa75c44fb077f3b29be22a101f244eb62ca6cd9
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-27 23:40:58 -0400

    added image to home page

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -16,6 +16,7 @@ class Home extends Component {
           <p>We aim to be New York's "one-stop-beauty-shop" by offering a full menu of hair, nail, eyelash, skin, and waxing services while having our most trusted products available for our clients to purchase. </p>
           <p>While internal beauty will always reign supreme, we wholeheartedly believe that when people look better on the outside, they feel better on the inside. If we can improve your quality of life through the services we provide, then we have completed our mission. </p>
           <p>You take care of the inner beauty... leave the rest to us. </p>
+          <img src={require('../../images/salon_main.jpg')} alt="Style_2" style={{width:600, height:413}} />
          </div>
 
          <div className="col_2">
