438622183ecb7b45dbc8c74ada189271b0dbf317
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-28 01:43:02 -0400

    added link to my page on Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,8 @@ const Footer = () => (
   <footer id="footer">
     <div id="footer_wrapper">
       <div className="footer_div">ALL COPYRIGHT RESERVED © 2017 SHEARS NYC SALON. SHEARS RESERVATIONS: (518) 392-3550 OR (212) 219-3195
-      <a id="link_to_top" href="#top">Back to top <i className="fa fa-angle-up" aria-hidden="true"></i></a></div>
+      <a id="link_to_top" href="#top">Back to top <i className="fa fa-angle-up" aria-hidden="true"/></a></div>
+      <div className="footer_div"><a id="link_to_me" href="https://mdjurdjevic.me"><i className="fa fa-share" aria-hidden="true"></i> Website by Michael Djurdjevic</a></div>
     </div>
   </footer>
 )
