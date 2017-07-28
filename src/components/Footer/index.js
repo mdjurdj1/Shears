51612c7433f25e8f78c88d89d128bdeaaef79ba8
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-28 01:28:07 -0400

    added sliding border for hovered nav link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 import './footer.css'
 
 const Footer = () => (
   <footer id="footer">
     <div id="footer_wrapper">
-      <div>hey</div>
-      <div>HAYY</div>
+      <div className="footer_div">ALL COPYRIGHT RESERVED © 2017 SHEARS NYC SALON. SHEARS RESERVATIONS: (518) 392-3550 OR (212) 219-3195
+      <a id="link_to_top" href="#top">Back to top <i className="fa fa-angle-up" aria-hidden="true"></i></a></div>
     </div>
   </footer>
 )
