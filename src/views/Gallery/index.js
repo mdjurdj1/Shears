43afe108dd888c68c96463850c2514e87f8ce96a
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-31 03:28:55 -0400

    swapped over to lightbox for gallery

--- src/views/Gallery/index.js
+++ src/views/Gallery/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom'
-import Modal from 'react-modal'
+import Lightbox from 'react-image-lightbox'
 
 import HoursContainer from '../../components/HoursContainer'
 import StoreInfoContainer from '../../components/StoreInfoContainer'
@@ -14,89 +14,108 @@ import img_6 from '../../images/img_6.jpeg'
 import img_7 from '../../images/img_7.jpg'
 import img_8 from '../../images/img_8.jpg'
 import img_9 from '../../images/img_9.jpg'
+import customStyle from './customStyle.js'
 import './gallery.css'
+
+const images = [
+    img_1_alt,
+    img_2,
+    img_3,
+    img_4,
+    img_5,
+    img_6,
+    img_7,
+    img_8
+];
 
 class Gallery extends Component {
   state = {
-    modalIsOpen: false,
-    current_image: false
+    lightBoxOpen: false,
+    photoIndex: 0,
   }
-  openModal = (src) => {
-    this.setState({modalIsOpen: true, current_image: src})
-  }
-  closeModal = () => {
-    this.setState({modalIsOpen: false, current_image: false})
+  openLightBox = (index) => {
+    this.setState({lightBoxOpen: true, photoIndex: index})
   }
   render() {
+    const {
+      photoIndex,
+      lightBoxOpen,
+      } = this.state;
+
     return (
        <div id="gallery_wrapper">
 
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onRequestClose={this.closeModal}
-          contentLabel="Example Modal"
-        >
+        {lightBoxOpen &&
+           <Lightbox
+               mainSrc={images[photoIndex]}
+               nextSrc={images[(photoIndex + 1) % images.length]}
+               prevSrc={images[(photoIndex + images.length - 1) % images.length]}
 
-          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-          <button onClick={this.closeModal}>close</button>
-          <div><img src={this.state.current_image} /></div>
-        </Modal>
+               onCloseRequest={() => this.setState({ lightBoxOpen: false })}
+               onMovePrevRequest={() => this.setState({
+                   photoIndex: (photoIndex + images.length - 1) % images.length,
+               })}
+               onMoveNextRequest={() => this.setState({
+                   photoIndex: (photoIndex + 1) % images.length,
+               })}
+           />
+           }
 
         <div className="col_1">
           <h1>Check out our gallery!</h1>
-          <p>Stylist responsible for each look is credited under the photo.</p>
+          <p>Click any photo for a closer look! <br />Stylist responsible for each look is credited under the photo.</p>
           <div className="responsive">
             <div className="gallery">
-                <img onClick={() => this.openModal(img_1_alt)} src={img_1} style={{width:140, height:140}} />
+                <img onClick={() => this.openLightBox(0)} src={img_1} style={{width:140, height:140}} />
               <div className="desc">Stylist: Aida</div>
             </div>
           </div>
 
           <div className="responsive">
             <div className="gallery">
-                <img onClick={() => this.openModal(img_9)} src={img_9} style={{width:140, height:140}} />
+                <img onClick={() => this.openLightBox(1)} src={img_9} style={{width:140, height:140}} />
               <div className="desc">Stylist: George</div>
             </div>
           </div>
 
           <div className="responsive">
             <div className="gallery">
-                <img onClick={() => this.openModal(img_6)} src={img_6} style={{width:140, height:140}} />
+                <img onClick={() => this.openLightBox(2)} src={img_6} style={{width:140, height:140}} />
               <div className="desc">Stylist: Nathalie</div>
             </div>
           </div>
 
           <div className="responsive">
             <div className="gallery">
-                <img onClick={() => this.openModal(img_7)} src={img_7} style={{width:140, height:140}} />
+                <img onClick={() => this.openLightBox(3)} src={img_7} style={{width:140, height:140}} />
               <div className="desc">Stylist: George</div>
             </div>
           </div>
 
           <div className="responsive">
             <div className="gallery">
-                <img onClick={() => this.openModal(img_4)} src={img_4} style={{width:140, height:140}} />
+                <img onClick={() => this.openLightBox(4)} src={img_4} style={{width:140, height:140}} />
               <div className="desc">Stylist: Alexis</div>
             </div>
           </div>
 
           <div className="responsive">
             <div className="gallery">
-                <img onClick={() => this.openModal(img_5)} src={img_5} style={{width:140, height:140}} />
+                <img onClick={() => this.openLightBox(5)} src={img_5} style={{width:140, height:140}} />
               <div className="desc">Stylist: Alexis</div>
             </div>
           </div>
 
           <div className="responsive">
             <div className="gallery">
-                <img onClick={() => this.openModal(img_3)} src={img_3} style={{width:140, height:140}} />
+                <img onClick={() => this.openLightBox(6)} src={img_3} style={{width:140, height:140}} />
               <div className="desc">Stylist: Alexis</div>
             </div>
           </div>
 
           <div className="responsive">
             <div className="gallery">
-                <img onClick={() => this.openModal(img_8)} src={img_8} style={{width:140, height:140}} />
+                <img onClick={() => this.openLightBox(7)} src={img_8} style={{width:140, height:140}} />
               <div className="desc">Stylist: Nathalie</div>
             </div>
           </div>
