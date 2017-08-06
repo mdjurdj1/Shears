5543be786e6c2a151f9eb66f8a1d36bb1b32ca1b
Mdjurdj1 <mdjurdj1@gmail.com>
2017-08-06 01:03:15 -0400

    fixed sidebar icon import

--- src/components/SocialSidebar/index.js
+++ src/components/SocialSidebar/index.js
@@ -4,11 +4,11 @@ import './sidebar.css'
 const Sidebar = () => (
   <aside id="sticky-social">
     <ul>
-        <li><a href="#" className="entypo-facebook" target="_blank"><span>Facebook</span></a></li>
-        <li><a href="#" className="entypo-twitter" target="_blank"><span>Twitter</span></a></li>
-        <li><a href="#" className="entypo-gplus" target="_blank"><span>Google+</span></a></li>
-        <li><a href="#" className="entypo-instagrem" target="_blank"><span>Instagram</span></a></li>
-        <li><a href="#" className="entypo-pinterest" target="_blank"><span>Pinterest</span></a></li>
+        <li><a href="#" className="entypo-facebook" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i><span>Facebook</span></a></li>
+        <li><a href="#" className="entypo-twitter" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i><span>Twitter</span></a></li>
+        <li><a href="#" className="entypo-gplus" target="_blank">  <i className="fa fa-google-plus" aria-hidden="true"></i><span>Google+</span></a></li>
+        <li><a href="#" className="entypo-pinterest" target="_blank"><i className="fa fa-pinterest-p" aria-hidden="true"></i><span>Pinterest</span></a></li>
+        <li><a href="#" className="entypo-instagrem" target="_blank"><i className="fa fa-instagram" aria-hidden="true"></i><span>Instagram</span></a></li>
     </ul>
 </aside>
 )
